fa0b11ff08082c658ca410caca61a3adda49ce27
Devernua <Kukuev@phystech.edu>
2016-12-18 13:42:30 +0300

    complite

--- AESCipher/AESCipher.py
+++ AESCipher/AESCipher.py
@@ -1,11 +1,6 @@
-import base64
 from Crypto.Cipher import AES
 from Crypto import Random
 from bigint.big import *
-
-BS = 16
-pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
-unpad = lambda s: s[:-ord(s[len(s)-1:])]
 
 
 class AESCipher:
@@ -13,16 +8,12 @@ class AESCipher:
         self.key = key
 
     def encrypt(self, raw):
-        #raw = pad(raw)
         iv = Random.new().read(AES.block_size)
         cipher = AES.new(self.key, AES.MODE_CFB, iv)
         return base64.standard_b64encode(iv + cipher.encrypt(raw))
-        #return str(unpack_bigint(iv + cipher.encrypt(raw))).encode()
 
     def decrypt(self, enc):
-            #enc = bytes(pack_bigint(int(enc)), )
             enc = base64.standard_b64decode(enc)
             iv = enc[:16]
             cipher = AES.new(self.key, AES.MODE_CFB, iv)
-            #return unpad(cipher.decrypt(enc[16:])).decode()
             return cipher.decrypt(enc[16:]).decode()
